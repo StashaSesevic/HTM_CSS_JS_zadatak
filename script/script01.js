87141a1347db32d05b67e27aafe5424e2666b64c
StashaSesevic <100646235+StashaSesevic@users.noreply.github.com>
2023-09-14 00:45:27 +0200

    Update script01.js

--- script/script01.js
+++ script/script01.js
@@ -17,33 +17,7 @@ $(document).ready(function() {
     });
   });
 
-  /*
-$("#" + odgovorID) je selektor koji koristi jQuery da pronađe HTML element koji ima ID
-  koji odgovara vrednosti promenljive odgovorID. Ovde se koristi znak # da bi se označilo da
-  tražimo element po ID-ju.Na primer, ako je odgovorID postavljen na "odgovor2",
-  onda će $("#" + odgovorID) pronaći HTML element sa ID-om "odgovor2",
-  ukoliko takav postoji na stranici. Ovaj kod će vratiti referencu na taj element,
-  koju možemo koristiti da manipulišemo njegovim stilovima, sadržajem ili drugim svojstvima.
-
-
-$("p[id^='pitanje']") je selektor jQuery koji bira sve paragrafe (<p>) koji
-    imaju ID koji počinje sa "pitanje".
-    Simbol ^ označava "počinje sa". Dakle, ovaj selektor će odabrati sve
-    paragrafe čiji ID počinje sa "pitanje".
-
-Funkcija unutar click() metode će se izvršiti kada se klikne na element
-     koji je odabran selektorom $("p[id^='pitanje']").
-
-     Unutar ove funkcije, prvo se nalazi ID odgovarajućeg odgovora tako što se
-     uzima vrednost atributa "id" paragrafa koji je kliknut ($(this).attr("id")).
-     Zatim se u ovom ID-ju zamenjuje početak "pitanje" sa "odgovor" kako bi se dobio ID odgovarajućeg odgovora.
-
-Na primer, ako je kliknuto na paragraf sa ID-om "pitanje1",
-odgovorID će postati "odgovor1". Ako je kliknuto na paragraf sa ID-om "pitanje2",
-odgovorID će postati "odgovor2", i tako dalje.
-
-*/
-
+ 
 // Funkcija koja izračunava preostalo vreme do otvaranja teretane
 function countdownTimer(targetDate) {
     var now = new Date().getTime();
@@ -74,16 +48,7 @@ function countdownTimer(targetDate) {
   // Pokreće odbrojavanje svake sekunde
   setInterval(updateCountdown, 1000);
 
-  /*Evo šta ovaj kod radi korak po korak:
-
-Definiše funkciju countdownTimer(targetDate) koja prima ciljani datum kao argument. Unutar ove funkcije, izračunava preostalo vreme između trenutnog vremena i ciljanog datuma u milisekundama.
-
-Izračunava preostalo vreme u danima, satima, minutima i sekundama.
-
-Funkcija updateCountdown() postavlja ciljni datum na "2023-10-01T12:00:00" (1. okt 2023. godine u 12:00 sati) i ažurira prikaz preostalog vremena na stranici.
-
-Koristi setInterval() funkciju koja poziva funkciju updateCountdown() svake sekunde kako bi ažurirala prikaz preostalog vremena. Ovo omogućava da se preostalo vreme prikazuje u realnom vremenu i automatski se ažurira svake sekunde.*/
-
+  
 //za burger meni(burger u x i obrnuto, otvaranje i zatvaranje menija)
 function Funkcija1(x) {
   x.classList.toggle("change");
